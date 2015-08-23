d853c6cf965f5e82d99ae6eb4cc0fb70496ad766
inetrica <inetrica@gmail.com>
2015-08-23 12:01:54 -0500

    challenge (reverse input)

--- OrderedWords.java
+++ OrderedWords.java
@@ -1,5 +1,11 @@
 import java.util.Scanner;
 
+/**
+ * This class prompts the user for a word.
+ * The user will be notified if the word is in alphabetical order, in reverse
+ * alphabetical order, or not in order
+ *
+ */
 public class OrderedWords {
     public static void main(String[] args){
         //input string
@@ -15,33 +21,74 @@ public class OrderedWords {
         while(!done){
             word = getInput(stdin);
 
-            boolean inOrder = isInOrder(word);
+            int inOrder = isInOrder(word);
 
-            if(inOrder){
+            if(inOrder == 0){
                 System.out.println("IN ORDER");
-            } else {
+            } else if (inOrder == 1) {
                 System.out.println("NOT IN ORDER");
+            } else if (inOrder == 2) {
+                System.out.println("REVERSE ORDER");
+            } else {
+                System.err.println("An error occured processing this word");
             }
             System.out.println();
         }
 
     }
 
-    private static boolean isInOrder(String word){
+    /**
+     *  in order returns 0
+     *  out of order returns 1
+     *  reverse order returns 2
+     */
+    private static int isInOrder(String word){
         //initial min value for 'a'
-        int prev = 'a' - 1;
+        int prev = 1;
+
+        //orders
+        boolean order, reverse;
+        order = true;
+        reverse = true;
+        
+        //iterate through the letters of the word, 
+        //determining whether they are in order
         for(int i = 0; i < word.length(); i++){
             int val = word.charAt(i);
-            
-            if(val < prev){
-                return false;
+            //if  its the first letter, nothing to compare yet
+            if(i==0){
+                prev = val;
+                continue;
             }
+            
+            //if the next char is smaller, its not in order
+            //if the next char is greater, its not in reverse order
+            if(val < prev){
+                order = false;
+            } else if (val > prev){
+                reverse = false;
+            }
+            
+            //if we've already determined its neither in order, nor in reverse
+            //just return as "not in order"
+            if(!order && !reverse){
+                return 1;
+            }
+
             prev = val;
         }
-        return true;
 
+        if(order) return 0;
+        if(reverse) return 2;
+
+        return -1;
     }
 
+    /**
+     *   use this function to get user input
+     *   if input is 'q!', we quit
+     *   if input contains anything besides letters, re-prompt for input
+     */
     private static String getInput(Scanner stdin){
         //TODO handle invalid input (only ints or symbols)
         boolean valid = true;
